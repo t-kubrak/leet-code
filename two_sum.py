50c0f4727d2cf05f4f19e377560bc3fc0f20255d
t-kubrak <chikashikaari@gmail.com>
2020-06-02 10:54:16 -0400

    ref two sum

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,4 @@
-def twoSum(nums, target):
+def two_sum_brute_force(nums, target):
     for key, num in enumerate(nums):
         for key_two, num_two in enumerate(nums[key + 1:]):
             if num + num_two == target:
@@ -6,6 +6,20 @@ def twoSum(nums, target):
 
     return None
 
-result = twoSum([2, 7, 11, 15], 26)
+def two_sum(nums, target):
+    dict = {}
+
+    for key, num in enumerate(nums):
+        num_two = target - num
+
+        if num_two not in dict:
+            dict[num] = key
+            continue
+
+        return [dict[num_two], key]
+
+    return None
+
+result = two_sum([2, 7, 11, 15], 18)
 
 print(result)

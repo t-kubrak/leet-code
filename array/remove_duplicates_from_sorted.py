41116a796a203c410c672edecf8fe3b6204a25b5
t-kubrak <chikashikaari@gmail.com>
2020-06-10 19:56:48 -0400

    remove duplicates from sorted array

---
+++ array/remove_duplicates_from_sorted.py
@@ -0,0 +1,22 @@
+def removeDuplicates(nums):
+    if len(nums) < 2:
+        return 1
+
+    unique_index = 1
+
+    for i in range(len(nums) - 1):
+        current = nums[i]
+        next = nums[i + 1]
+
+        if current != next:
+            nums[unique_index] = next
+            unique_index += 1
+
+    nums = nums[:unique_index]
+
+    return unique_index
+
+result = removeDuplicates([0, 1, 1, 1, 2])
+
+print(result)
+

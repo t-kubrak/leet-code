529d70fef0912509f8c0af8d2e860fddb43c89b8
t-kubrak <chikashikaari@gmail.com>
2020-06-22 17:07:32 -0400

    plus one, other versions

--- array/plus_one.py
+++ array/plus_one.py
@@ -14,6 +14,28 @@ def plus_one(digits: List[int]) -> List[int]:
     return list(map(lambda num: int(num), number))
 
 
-result = plus_one([9])
-print(result)
+def plus_one_v_2(digits: List[int]) -> List[int]:
+    if len(digits) == 0:  # one digit case
+        digits = [1]
+    elif digits[-1] == 9:
+        digits = plus_one_v_2(digits[:-1])
+        digits.append(0)
+    else:
+        digits[-1] += 1
 
+    return digits
+
+
+def plus_one_v_3(digits: List[int]) -> List[int]:
+    for i in range(len(digits)):
+        if digits[~i] < 9:
+            digits[~i] += 1
+            return digits
+
+        digits[~i] = 0
+
+    return [1] + digits
+
+
+result = plus_one([9, 9])
+print(result)

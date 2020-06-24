6cbcf39b6b6b993127d8e9c41e18cc1223fece8f
t-kubrak <chikashikaari@gmail.com>
2020-06-23 20:44:47 -0400

    rotate image; fix number of rotations depending on matrix size

--- array/rotate_image.py
+++ array/rotate_image.py
@@ -3,19 +3,24 @@ from typing import List
 
 def rotate(matrix: List[List[int]]):
     rows = len(matrix)
-    offsets = 0
 
-    while rows - offsets > 3:
-        offsets += 1
+    if rows > 3:
+        offsets = rows // 2
 
-    for offset in range(offsets + 1):
-        top_row = matrix[0 + offset][:]
-        right_col = to_right(matrix, top_row, offset)
-        bottom_row = to_bottom(matrix, right_col, offset)
-        left_col = to_left(matrix, bottom_row, offset)
-        to_top(matrix, left_col, offset)
+        for offset in range(0, offsets):
+            rotate_matrix(matrix, offset)
+    else:
+        rotate_matrix(matrix)
 
     return matrix
+
+
+def rotate_matrix(matrix: List[List[int]], offset: int = 0):
+    top_row = matrix[0 + offset][:]
+    right_col = to_right(matrix, top_row, offset)
+    bottom_row = to_bottom(matrix, right_col, offset)
+    left_col = to_left(matrix, bottom_row, offset)
+    to_top(matrix, left_col, offset)
 
 
 def to_right(matrix, top_row, offset):
@@ -75,7 +80,7 @@ matrix_2_2: List[List[int]] = [
     [3, 4]
 ]
 
-matrix: List[List[int]] = [
+matrix_3_3: List[List[int]] = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
@@ -96,5 +101,14 @@ matrix_5_5 = [
     [21, 22, 23, 24, 25]
 ]
 
-result = rotate(matrix_5_5)
+matrix_6_6 = [
+    [1, 2, 3, 4, 5, 6],
+    [7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18],
+    [19, 20, 21, 22, 23, 24, ],
+    [25, 26, 27, 28, 29, 30],
+    [31, 32, 33, 34, 35, 36]
+]
+
+result = rotate(matrix_4_4)
 print(result)

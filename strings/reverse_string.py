8e9f62caa5d1a610706930e0e16abda17b1bf420
t-kubrak <chikashikaari@gmail.com>
2020-06-24 16:01:48 -0400

    reverse string; other solutions

--- strings/reverse_string.py
+++ strings/reverse_string.py
@@ -1,11 +1,27 @@
 from typing import List
 
 
-def reverse_string(s: List[str]):
+def reverse_string(s: List[str]) -> None:
     for i in range(len(s) // 2):
         s[i], s[~i] = s[~i], s[i]
 
 
+def reverse_string_2(s: List[str]) -> None:
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left, right = left + 1, right - 1
+
+
+def reverse_string_3(s: List[str]) -> None:
+    def reverse(left, right):
+        if left < right:
+            s[left], s[right] = s[right], s[left]
+            reverse(left + 1, right - 1)
+
+    reverse(0, len(s) - 1)
+
 input = ["H", "a", "n", "n", "a", "h"]
-result = reverse_string(input)
+result = reverse_string_3(input)
 print(result)
